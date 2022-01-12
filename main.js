1ea86809c69091b272408e68b17bcd6ab2adc6c9
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-11 21:27:54 -0600

    add

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Hamburger menu
 const bar = document.getElementById('bar');
 const navbar = document.getElementById('navbar');
 const closeMenu = document.getElementById('close');
@@ -12,4 +13,18 @@ closeMenu.addEventListener('click', () => {
     navbar.classList.remove('active');
 })
 
+// Alert for app installation
+let appImage = document.getElementById('appStore');
+let playImage = document.getElementById('playStore');
+
+appImage.addEventListener('click', () => {
+    alert('Go to App Store');
+    window.location.href = window.open("https://www.apple.com/app-store/");
+})
+
+playImage.addEventListener('click', () => {
+    alert('Go to Google Play Store');
+    window.location.href = window.open("https://play.google.com/store", "_blank");
+
+})
 
